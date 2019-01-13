011b8281fb41159ffed2717189f18b902ceeb2cd
kkoomin <minha.koo.9@gmail.com>
2019-01-13 21:11:19 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |el|
+    if new_hash[el]
+      new_hash[el] += 1
+    else
+      new_hash[el] = 1
+    end
+  end
+  new_hash
 end
- 
+
+# binding.pry
